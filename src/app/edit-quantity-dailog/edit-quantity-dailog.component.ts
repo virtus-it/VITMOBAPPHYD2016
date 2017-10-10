22d9022c80805a529c392d78f73509ef2bdd8c80
firasath-virtus <firasath.ali@virtus-it.com>
2017-10-10 19:11:19 +0530

    Add edit quantity dailog

---
+++ src/app/edit-quantity-dailog/edit-quantity-dailog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-quantity-dailog',
+  templateUrl: './edit-quantity-dailog.component.html',
+  styleUrls: ['./edit-quantity-dailog.component.css']
+})
+export class EditQuantityDailogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
