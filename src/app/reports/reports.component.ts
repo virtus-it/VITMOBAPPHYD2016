fc31f9056833b687a3669acfecd7cd847b871908
arif <arif.shareef@flolive.net>
2017-11-07 12:51:27 +0530

    Reports downloaded but not orders

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../login/authentication.service';
 import { ReportsService } from './reports.service';
+import * as _ from 'underscore';
 @Component({
 
   templateUrl: './reports.component.html',
@@ -10,18 +11,33 @@ export class ReportsComponent implements OnInit {
 
   constructor(private authenticationService: AuthenticationService, private reportservice: ReportsService) { }
   reportDetails = { reportType: "", days: "", lastId: "0", pagesize: 100, appType: this.authenticationService.appType() };
+  reportsClickMore:boolean = false;
   reportsInput: any = {};
   reportsData = [];
   reportsType = [
     { value: 'newlydownloaded', viewValue: 'Newly Downloded' },
     { value: 'lastdays', viewValue: 'Last Days' },
     { value: 'pendingorders', viewValue: 'Pending Orders' },
-    { value: 'rejectedorders', viewValue: 'Rejected' },
-
+    { value: 'rejectedorders', viewValue: 'Rejected Orders' },
+    { value: 'onlydownloaded', viewValue: 'Only Downloaded' },
+    { value: 'notregistered', viewValue: 'Not Registered' },
+    
   ];
-  searchReports() {
+  searchReports(firstCall) {
     this.reportsInput = JSON.parse(JSON.stringify(this.reportDetails));
-    let input = { "root": { "days": this.reportsInput.days, "lastid": this.reportsInput.lastId, "pagesize": this.reportsInput.pagesize, "transtype": this.reportsInput.reportType,"userid": this.authenticationService.loggedInUserId(),"apptype": this.authenticationService.appType() } }
+    let input = { "root": { "days": this.reportsInput.days, "lastid": this.reportsInput.lastId, "pagesize": this.reportsInput.pagesize, "transtype": this.reportsInput.reportType, "userid": this.authenticationService.loggedInUserId(), "apptype": this.authenticationService.appType() } };
+    if (this.reportsData && this.reportsData.length && !firstCall) {
+      let lastRecord: any = _.last(this.reportsData);
+      if (lastRecord) {
+        input.root.lastid = lastRecord.user_id;
+      }
+      
+
+    }
+    else{
+      this.reportsData = [];
+      input.root.lastid = null;
+    }
     this.reportservice.searchReports(input)
       .subscribe(
       output => this.searchReportsResult(output),
@@ -30,8 +46,14 @@ export class ReportsComponent implements OnInit {
       });
   }
   searchReportsResult(result) {
+    if (result.data && result.data.output && result.data.output.length > 0) {
+      this.reportsClickMore = true;
+      this.reportsData = _.union(this.reportsData,result.data.output);
+   }
+   else{
+    this.reportsClickMore = false;
+   }
 
-    this.reportsData = result.data.output;
 
   }
 
