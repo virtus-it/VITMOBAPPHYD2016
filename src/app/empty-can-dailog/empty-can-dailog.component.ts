4e442ec9fdc0a4108d0c56e86b6faf78600cac7f
arif <arif.shareef@flolive.net>
2017-11-09 20:07:23 +0530

    orders page

--- src/app/empty-can-dailog/empty-can-dailog.component.ts
+++ src/app/empty-can-dailog/empty-can-dailog.component.ts
@@ -27,6 +27,7 @@ export class EmptyCanDailogComponent implements OnInit {
   getProductsResult(result) {
     console.log(result);
     if (result.data && result.data.length > 0) {
+      let orderdata = this.orderDetail;
       let canFormat = { "root": { "brandname":"","brandType":"","avaliablecans": "", "loginid": this.authenticationService.loggedInUserId(), "customerid": this.orderDetail.order_by, "dealerid": "289", "productid": "", "apptype": this.authenticationService.appType(), "createdthru": "website" } };
       let cansData = [];
       _.each(result.data, function (i, j) {
@@ -34,6 +35,7 @@ export class EmptyCanDailogComponent implements OnInit {
         let canFormatcopy = JSON.parse(JSON.stringify(canFormat));
         canFormatcopy.root.brandname = details.brandname;
         canFormatcopy.root.brandType = details.ptype;
+        
         canFormatcopy.root.avaliablecans = details.avaliable_emptycans;
         canFormatcopy.root.productid = details.productid;
         cansData.push(canFormatcopy);
