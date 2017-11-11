ca30ad2a9b695d3a95460e6967f68cce87f59625
arif <arif.shareef@flolive.net>
2017-11-11 18:02:21 +0530

    sending mesages

--- src/app/order-detail-dailog/order-detail-dailog.component.ts
+++ src/app/order-detail-dailog/order-detail-dailog.component.ts
@@ -19,102 +19,119 @@ export class OrderDetailDailogComponent implements OnInit {
     constructor(private authenticationService: AuthenticationService, public thisDialogRef: MdDialogRef<OrderDetailDailogComponent>, @Inject(MD_DIALOG_DATA) public orderDetail: any, public dialog: MdDialog, private orderLandingService: OrderLandingService) { }
     dailogOrderDetails: any = {};
     customerProductDetails: any = [];
-    showCustomerDetails(orderData) {
-        let dialogRefEditCustomer = this.dialog.open(CustomerDetailDailogComponent, {
-            width: '95%',
-            data: orderData
-        });
-        dialogRefEditCustomer.afterClosed().subscribe(result => {
-            console.log(`Dialog closed: ${result}`);
+    messageInput = {"order":{ "orderstatus":"Message", "usertype":this.authenticationService.userType(), "loginid":this.authenticationService.loggedInUserId(), "orderid":this.orderDetail.order_id, "ispublic":"0", "customerid":this.orderDetail.order_by, "reason":"" } };
+showCustomerDetails(orderData) {
+    let dialogRefEditCustomer = this.dialog.open(CustomerDetailDailogComponent, {
+        width: '95%',
+        data: orderData
+    });
+    dialogRefEditCustomer.afterClosed().subscribe(result => {
+        console.log(`Dialog closed: ${result}`);
 
 
-        });
+    });
 
-    }
-    editCan(orderData) {
-        this.customerProductDetails.order_by = orderData.order_by;
-        let dialogRefEditCan = this.dialog.open(EmptyCanDailogComponent, {
-            width: '700px',
-            data: this.customerProductDetails
-        });
-        dialogRefEditCan.afterClosed().subscribe(result => {
-            console.log(`Dialog closed: ${result}`);
-            if (result == 'success') {
-                this.getProductsListByCustomerId();
-            }
-
-        });
-
-    }
-    onHoldStatus(orderData) {
-        let dialogRefonHoldStatus = this.dialog.open(OnHoldOrderStatusComponent, {
-            width: '550px',
-            data: orderData
-        });
-        dialogRefonHoldStatus.afterClosed().subscribe(result => {
-            console.log(`Dialog closed: ${result}`);
-            if (result == 'success') {
-                this.getOrderDetailsById();
-            }
-
-        });
-
-    }
-    editStatus(orderData) {
-        let dialogRefeditStatus = this.dialog.open(EditOrderStatusComponent, {
-            width: '750px',
-            data: orderData
-        });
-        dialogRefeditStatus.afterClosed().subscribe(result => {
-            console.log(`Dialog closed: ${result}`);
-            if (result == 'success') {
-                this.getOrderDetailsById();
-                this.getProductsListByCustomerId();
-            }
-
-        });
-
-    }
-    getOrderDetailsById() {
-        console.log(this.orderDetail);
-        let input = { orderId: this.orderDetail.order_id, appType: this.authenticationService.appType(), userId: this.authenticationService.loggedInUserId() };
-        this.orderLandingService.getOrderById(input)
-            .subscribe(
-            output => this.getOrderDetailsByIdResult(output),
-            error => {
-                console.log("error in order details");
-            });
-    }
-    getOrderDetailsByIdResult(result) {
-        console.log(result);
-        if (result.data && result.data.length > 0) {
-            this.dailogOrderDetails = result.data[0];
+}
+editCan(orderData) {
+    this.customerProductDetails.order_by = orderData.order_by;
+    let dialogRefEditCan = this.dialog.open(EmptyCanDailogComponent, {
+        width: '700px',
+        data: this.customerProductDetails
+    });
+    dialogRefEditCan.afterClosed().subscribe(result => {
+        console.log(`Dialog closed: ${result}`);
+        if (result == 'success') {
+            this.getProductsListByCustomerId();
         }
-    }
-    getProductsListByCustomerId() {
-        let input = { customerID: this.orderDetail.order_by, appType: this.authenticationService.appType() };
-        this.orderLandingService.getProductsByCustomerID(input)
-            .subscribe(
-            output => this.getProductsListByCustomerIdResult(output),
-            error => {
-                console.log("error in order details");
-            });
 
-    }
-    getProductsListByCustomerIdResult(result) {
+    });
 
-        if (result.data.user.stock && result.data.user.stock.length > 0) {
-            console.log(result.data.user.stock);
-            this.customerProductDetails = _.filter(result.data.user.stock, function (e: any) { return e.avaliablecans !== 0; });
-
+}
+onHoldStatus(orderData) {
+    let dialogRefonHoldStatus = this.dialog.open(OnHoldOrderStatusComponent, {
+        width: '550px',
+        data: orderData
+    });
+    dialogRefonHoldStatus.afterClosed().subscribe(result => {
+        console.log(`Dialog closed: ${result}`);
+        if (result == 'success') {
+            this.getOrderDetailsById();
         }
+
+    });
+
+}
+editStatus(orderData) {
+    let dialogRefeditStatus = this.dialog.open(EditOrderStatusComponent, {
+        width: '750px',
+        data: orderData
+    });
+    dialogRefeditStatus.afterClosed().subscribe(result => {
+        console.log(`Dialog closed: ${result}`);
+        if (result == 'success') {
+            this.getOrderDetailsById();
+            this.getProductsListByCustomerId();
+        }
+
+    });
+
+}
+getOrderDetailsById() {
+    console.log(this.orderDetail);
+    let input = { orderId: this.orderDetail.order_id, appType: this.authenticationService.appType(), userId: this.authenticationService.loggedInUserId() };
+    this.orderLandingService.getOrderById(input)
+        .subscribe(
+        output => this.getOrderDetailsByIdResult(output),
+        error => {
+            console.log("error in order details");
+        });
+}
+getOrderDetailsByIdResult(result) {
+    console.log(result);
+    if (result.data && result.data.length > 0) {
+        this.dailogOrderDetails = result.data[0];
     }
-    onCloseCancel() {
-        this.thisDialogRef.close('Cancel');
+}
+getProductsListByCustomerId() {
+    let input = { customerID: this.orderDetail.order_by, appType: this.authenticationService.appType() };
+    this.orderLandingService.getProductsByCustomerID(input)
+        .subscribe(
+        output => this.getProductsListByCustomerIdResult(output),
+        error => {
+            console.log("error in order details");
+        });
+
+}
+getProductsListByCustomerIdResult(result) {
+
+    if (result.data.user.stock && result.data.user.stock.length > 0) {
+        console.log(result.data.user.stock);
+        this.customerProductDetails = _.filter(result.data.user.stock, function (e: any) { return e.avaliablecans !== 0; });
+
     }
-    ngOnInit() {
+}
+sendMessage(){
+    let input = this.messageInput;
+    this.orderLandingService.sendMessage(input)
+    .subscribe(
+    output => this.sendMessageResult(output),
+    error => {
+        console.log("error in order details");
+    });
+}
+sendMessageResult(result){
+    console.log(result);
+    if(result.result == 'success'){
+        this.messageInput.order.reason = "";
         this.getOrderDetailsById();
-        this.getProductsListByCustomerId();
     }
+}
+onCloseCancel() {
+    this.thisDialogRef.close('Cancel');
+}
+ngOnInit() {
+    this.getOrderDetailsById();
+    this.getProductsListByCustomerId();
+}
 
 }
