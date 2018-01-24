1314f1f6b78957a43a069c845003de5d84b111c2
arif <arif.shareef@flolive.net>
2018-01-24 20:18:18 +0530

    add stocks changes

--- src/app/add-stock-history/add-stock-history.component.ts
+++ src/app/add-stock-history/add-stock-history.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+import { AuthenticationService } from '../login/authentication.service';
+import { ProductsService } from '../products/products.service';
+import { LoaderService } from '../login/loader.service';
+import { MdDialogRef } from '@angular/material';
 @Component({
   selector: 'app-add-stock-history',
   templateUrl: './add-stock-history.component.html',
@@ -7,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddStockHistoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService, private productService: ProductsService, public thisDialogRef: MdDialogRef<AddStockHistoryComponent>) { }
+StockList=[];
+  getStockHistroy() {
+    let input = {
+      "root": {
+        "userid": this.authenticationService.loggedInUserId(), "usertype": this.authenticationService.userType(), "pid": "0", "areaid": "", "last_historyid": 100000, "pagesize": "100",
+        "fromdate": null, "todate": null, "apptype": this.authenticationService.appType()
+      }
+    };
+    this.productService.getStockHistroy(input)
+      .subscribe(
+      output => this.getStockHistroyResult(output),
+      error => {
+        console.log("error in stock histroy");
 
-  
+      });
+  }
+  getStockHistroyResult(result) {
+    console.log(result);
+    if(result.result == 'success'){
+      this.StockList = result.data;
+    }
+  }
+  onCloseModal() {
+    this.thisDialogRef.close('cancel');
+  }
   ngOnInit() {
+    this.getStockHistroy();
   }
 
 }
