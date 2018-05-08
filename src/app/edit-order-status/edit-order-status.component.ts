62d63cac7ca295f8947273c8fedfc4ba07f7110f
unknown <firasath.ali@virtus-it.com>
2018-05-08 13:59:59 +0530

    Added amount

--- src/app/edit-order-status/edit-order-status.component.ts
+++ src/app/edit-order-status/edit-order-status.component.ts
@@ -13,11 +13,13 @@ import { LoaderService } from '../login/loader.service';
 })
 export class EditOrderStatusComponent implements OnInit {
 
-  constructor(private authenticationService: AuthenticationService, public thisDialogRef: MdDialogRef<EditOrderStatusComponent>, @Inject(MD_DIALOG_DATA) public orderDetail: any, public dialog: MdDialog, private orderLandingService: OrderLandingService,private loaderService: LoaderService) { }
+  constructor(private authenticationService: AuthenticationService, public thisDialogRef: MdDialogRef<EditOrderStatusComponent>, @Inject(MD_DIALOG_DATA) public orderDetail: any, public dialog: MdDialog, private orderLandingService: OrderLandingService,private loaderService: LoaderService) {
+    
+   }
 
   
   editStatusInput:any = { "order": { "delivered_qty": this.orderDetail.delivered_quantity, "received_amt": (this.orderDetail.delivered_quantity * this.orderDetail.prod_cost)  + (this.orderDetail.delivered_quantity * this.orderDetail.servicecharges) + (this.orderDetail.expressdeliverycharges) , "orderstatus": "delivered", 
-  "reason":"",  "product_type": "cans", "loginid": this.authenticationService.loggedInUserId(), "orderid": this.orderDetail.order_id, "usertype": this.authenticationService.userType(), "apptype": this.authenticationService.appType(), "return_cans": this.orderDetail.return_cans , "adv_amt":this.orderDetail.customerpaymentdts.advance_amount } };
+  "reason":"",  "product_type": "cans", "loginid": this.authenticationService.loggedInUserId(), "orderid": this.orderDetail.order_id, "usertype": this.authenticationService.userType(), "apptype": this.authenticationService.appType(), "return_cans": this.orderDetail.return_cans , "adv_amt": 0 } };
 isConfirmed = true;
 
 
@@ -113,6 +115,7 @@ if(result.result == 'success'){
     //console.log(this.editStatusInput);
     this.editStatusInput.order.paymentype = 'cash';
     this.editStatusInput.order.bill_amount = this.orderDetail.bill_amount;
+    
     console.log(this.orderDetail);
   }
 
