d9e9c8e4a7fd6d90c273415379e3522a08420bb6
arif <arif.shareef@flolive.net>
2018-06-05 12:38:36 +0530

    pointspage done

--- src/app/points/points.component.ts
+++ src/app/points/points.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EditPointsComponent } from '../edit-points/edit-points.component';
 import { DistributorServiceService } from '../distributor/distributor-service.service';
 import { MdDialog } from '@angular/material';
+import * as _ from 'underscore';
 
 
 @Component({
@@ -14,6 +15,14 @@ export class PointsComponent implements OnInit {
   constructor(public dialog: MdDialog, private distributorService: DistributorServiceService) { }
 
   pointsStatus:any = [];
+
+  referalbonusState1:any = '';
+  acceptStatus1:any = '';
+  deliveredStatus1:any = '';
+  signupStatus1:any = '';
+  sameDayStatus1:any = '';
+  ontimeStatus1:any = ''; 
+  distCustomerStatus1:any = '';
 
   DeactivatePoints(status){
     console.log('de activate called');
@@ -28,7 +37,7 @@ export class PointsComponent implements OnInit {
   }
   DeactivatePointsResult(result){
     if(result.result == 'success'){
-      // this.getPointsDetails();
+      this.getPointsDetails();
     }
   }
 
@@ -44,7 +53,7 @@ export class PointsComponent implements OnInit {
   }
   ActivatePointsResult(result){
     if(result.result == 'success'){
-      // this.getPointsDetails();
+      this.getPointsDetails();
 
     }
   }
@@ -55,15 +64,62 @@ export class PointsComponent implements OnInit {
     let input = {"User":{"TransType":"getallpoints"}};
     this.distributorService.getPoints(input)
     .subscribe(
-    output => this.DeactivatePointsResult(output),
+    output => this.getPointsDetailsResult(output),
     error => {    
       console.log('Error in getting all points Details');  
     });
   }
-  getAllPointsDetailsResult(result){
+
+  
+  getPointsDetailsResult(result){
     if(result.result == 'success'){
       this.pointsStatus = result.data;
-      console.log('result' , result.data);
+      console.log(result.data);
+      let referalbonusState:any = '';
+      let acceptStatus:any = '';
+      let deliveredStatus:any = '';
+      let signupStatus:any = '';
+      let sameDayStatus:any = '';
+      let ontimeStatus:any = ''; 
+      let distCustomerStatus:any = '';
+
+      _.each(this.pointsStatus , function(i , j){
+        let details:any = i;
+        if(details.status == 'referalbonus'){
+          referalbonusState = details.isactive; 
+        }
+        else if(details.status == 'accept'){ 
+          acceptStatus = details.isactive;
+        }
+        else if(details.status == 'delivered'){
+          deliveredStatus = details.isactive;
+        }
+        else if(details.status == 'signup'){
+          signupStatus = details.isactive;
+        }
+        else if(details.status == 'sameday'){
+          sameDayStatus = details.isactive;
+        }
+        else if(details.status == 'ontime'){
+          ontimeStatus = details.isactive;
+        }
+        else if(details.status == 'D-customer'){
+          distCustomerStatus = details.isactive;
+        }
+       
+      });
+
+      this.referalbonusState1 = referalbonusState;
+      this.acceptStatus1 = acceptStatus;
+      this.deliveredStatus1 =  deliveredStatus;
+      this.signupStatus1 = signupStatus;
+      this.sameDayStatus1 = sameDayStatus;
+      this.ontimeStatus1 = ontimeStatus;
+      this.distCustomerStatus1 =  distCustomerStatus;
+
+
+
+
     }
 
   }
