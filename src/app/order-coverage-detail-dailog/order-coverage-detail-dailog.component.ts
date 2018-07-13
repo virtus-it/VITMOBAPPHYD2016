e449689f344780df25621da88b0422df16903f62
unknown <Administrator@TESTER17.nms.com>
2018-07-13 17:58:09 +0530

    new bug

--- src/app/order-coverage-detail-dailog/order-coverage-detail-dailog.component.ts
+++ src/app/order-coverage-detail-dailog/order-coverage-detail-dailog.component.ts
@@ -643,25 +643,4 @@ export class OrderCoverageDetailDailogComponent implements OnInit {
 }
 
 
-
-// getPathsinFloat(){
-//   this.polyArray = [];
-//   let path = [];
-//   this.polyArray = this.authenticationService.getPolygons();
-//   _.each(this.polyArray , function(i, j){
-//     let details:any = i;
-//     path = details.path;
-  
-//   _.each(path , function(k ,l){
-//     let pathData:any = k;
-//     for(let latandlong of pathData){
-//       pathData.lat = parseFloat(pathData.lat);
-//       pathData.lng = parseFloat(pathData.lng);
-//     }
-//     path.push(pathData);
-//   });
-//   });
-//   this.polyArray.push(path);
-//   this.polygonArray = this.polyArray;
-//   }
   
