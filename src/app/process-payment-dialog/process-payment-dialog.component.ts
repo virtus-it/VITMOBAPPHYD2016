c9629fa1742bdbeea200a568fd593abb0c8565f5
ankush332 <ankush.ranapure@virtus-it.com>
2018-11-16 17:45:55 +0530

    send sms

--- src/app/process-payment-dialog/process-payment-dialog.component.ts
+++ src/app/process-payment-dialog/process-payment-dialog.component.ts
@@ -3,6 +3,7 @@ import { MdDialogRef } from '@angular/material';
 import { AuthenticationService } from '../login/authentication.service';
 import { MD_DIALOG_DATA } from '@angular/material';
 import { DistributorServiceService } from '../distributor/distributor-service.service';
+import { SmsServiceService } from '../sms/sms-service.service';
 
 @Component({
   selector: 'app-process-payment-dialog',
@@ -11,13 +12,17 @@ import { DistributorServiceService } from '../distributor/distributor-service.se
 })
 export class ProcessPaymentDialogComponent implements OnInit {
 
-  constructor( public thisDialogRef: MdDialogRef<ProcessPaymentDialogComponent>, private authenticationService: AuthenticationService,  @Inject(MD_DIALOG_DATA) public Details: any, private distributorService: DistributorServiceService  ) { }
+  constructor( public thisDialogRef: MdDialogRef<ProcessPaymentDialogComponent>, private authenticationService: AuthenticationService,  @Inject(MD_DIALOG_DATA) public Details: any, private distributorService: DistributorServiceService  , private smsService: SmsServiceService) { }
 
 
   processPaymentInput = {name:"" , type:"paytm", ackId:"" , paytmNumber:"" , amount: 0 }
+  smsObject = {name : '' , mobileno: '' , messageBody : ''};
 
   processPayment(){
     let input = {"User" : {"id": this.Details.id, loginid: this.authenticationService.loggedInUserId() , apptype:this.authenticationService.appType() , userid: this.Details.user_id , type : this.processPaymentInput.type , TransType:"updateprocess" ,  redeemmobileno : this.processPaymentInput.paytmNumber , acknowledgeid :  this.processPaymentInput.ackId , amount: this.processPaymentInput.amount }};
+    this.smsObject.name = this.processPaymentInput.name;
+    this.smsObject.mobileno = input.User.redeemmobileno;
+    this.smsObject.messageBody = 'message text ' + input.User.acknowledgeid + ' message text';
     console.log(input);
     this.distributorService.getPoints(input)
     .subscribe(
@@ -28,8 +33,27 @@ export class ProcessPaymentDialogComponent implements OnInit {
   processPaymentResult(result){
     if(result.result == 'success'){
       this.thisDialogRef.close('success');
+      this.sendSmsOfAcknowledgement(this.smsObject);
     }
   }
+
+  sendSmsOfAcknowledgement(data){
+    console.log(data , 'smsmsmsmsmsms');
+    let input = {"type":"","getAllMobileInput":{},"sendSmsInput":{"User":{"mobilenumber":[{"mobileno": data.mobileno,"gcm_regid":"","fullName":data.name}],"count":1,"name":"","smstype":"sms","user_type": this.authenticationService.userType() ,"transtype":"createsms","type":"","showcomment":false,"loginid": this.authenticationService.loggedInUserId() ,"apptype": this.authenticationService.appType() ,"body": data.messageBody ,"title":"","redirecturl":"","url":"","buttons":[""],"buttonactions":[],"option":[""],"sliderurl":[{"image":"","count":0}]}}}
+    console.log(input, 'final ip');
+    this.smsService.CreateSms(input)
+      .subscribe(
+        output => this.sendSmsOfAcknowledgementResult(output),
+        error => {
+        });
+  }
+  sendSmsOfAcknowledgementResult(result){
+    if(result && result.result == 'success'){
+      console.log('SMS sent successfully')
+    }
+
+  }
+
 
   getDetails(){
     if(this.Details){
