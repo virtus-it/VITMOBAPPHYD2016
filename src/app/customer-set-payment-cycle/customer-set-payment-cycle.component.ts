7a6dd049eec1168b0e4192bee106f1182cd4e664
ankush332 <ankush.ranapure@virtus-it.com>
2018-11-20 12:05:34 +0530

    set payment cycle

--- src/app/customer-set-payment-cycle/customer-set-payment-cycle.component.ts
+++ src/app/customer-set-payment-cycle/customer-set-payment-cycle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Inject} from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { MdDialogRef } from '@angular/material';
 import { MD_DIALOG_DATA } from '@angular/material';
 import { FormControl, Validators } from '@angular/forms';
@@ -17,104 +17,26 @@ export class CustomerSetPaymentCycleComponent implements OnInit {
   checkAll: boolean = false;
   checkAllWeek: boolean = false;
   checkAllDay: boolean = false;
-  selectAllWeekDays : boolean = false;
+  selectAllWeekDays: boolean = false;
   selectAllDays: boolean = false;
   setPaymentFormControl = new FormControl('', [Validators.required]);
 
-  setPaymentInput:any = { schedulefor: "weekdays" , CustomerName:"" , weekdays:""  , days:"" , timeslot: "7AM-8AM" , setCycle:"oneCustomer"}
+  setPaymentInput: any = { schedulefor: "weekdays", CustomerName: "", weekdays: "", days: "", timeslot: "7AM-8AM", setCycle: "oneCustomer" }
+
+  weekday : any = '';
 
 
-
-  //check all weekdays
-  onChangeCheckAll(isChecked: boolean) {
-
-
-    if (isChecked) {
-      
-      this.checkAllWeek = true;
-      this.setPaymentInput.weekdays="Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday";
-
-    } else {
-      this.checkAll = false;
-      this.checkAllWeek = false;
-      this.setPaymentInput.weekdays="";
-
-
+  setPaymentCycle() {
+    let input = { username: this.Detail.firstname , userid: this.Detail.userid , paymentDateType: this.setPaymentInput.schedulefor, paymentDay: '', timeslot: this.setPaymentInput.timeslot, paymentCycle: this.setPaymentInput.setCycle }
+    if(input.paymentDateType == 'weekdays'){
+      input.paymentDay = this.setPaymentInput.weekdays;
     }
+    else if(input.paymentDateType == 'days'){
+      input.paymentDay = this.setPaymentInput.days;
+    }
+    console.log(input, 'ipipip');
   }
-
-  //check for particular weekday
-  onChangeCheckWeek(week:string,  isChecked: boolean) {
-
-
-    if (isChecked) {
-      this.checkAll = false;
-      if(this.setPaymentInput.weekdays){
-        this.setPaymentInput.weekdays= this.setPaymentInput.weekdays +','  + week;
-
-      }
-      else{
-      this.setPaymentInput.weekdays= this.setPaymentInput.weekdays  + week;
-      }
-      
- 
-    } else {
-      this.checkAll = false;
-      this.selectAllWeekDays= false;
-
-    let replaceValue = this.setPaymentInput.weekdays.replace(new RegExp(week+',', 'g'), '');
-    replaceValue = this.setPaymentInput.weekdays.replace(new RegExp(week, 'g'), '');
-    this.setPaymentInput.weekdays = replaceValue;
-    }
-  }
-
-  // check for particular day
-  onChangeCheckDay(day: any, isChecked: boolean) {
-
-
-    if (isChecked) {
-      this.checkAll = false;
-      if(this.setPaymentInput.days){
-        this.setPaymentInput.days= this.setPaymentInput.days + ',' + day;
-      }
-      else{
-        this.setPaymentInput.days= this.setPaymentInput.days  + day;
-        }
-     
-    } else {
-      this.checkAll = false;
-      this.selectAllDays= false;
-      let replaceValue = this.setPaymentInput.days.replace(new RegExp(day+',', 'g'), '');
-      replaceValue = this.setPaymentInput.days.replace(new RegExp(day, 'g'), '');
-      this.setPaymentInput.days = replaceValue;
-     
-    }
-    let toBeSort:string  = this.setPaymentInput.days; // making the in string datatype
-    let sortedDays = toBeSort.split(",").sort().join(",");  //it should be string if we want to split it in typscript and soritng
-   
-    this.setPaymentInput.days = sortedDays;
-  }
-
-
-    //Check all days
-    onChangeCheckAllDays(isChecked: boolean) {
-
-
-      if (isChecked) {
-        
-        this.checkAllDay = true;
-        this.setPaymentInput.days="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31";
-  
-      } else {
-        this.checkAll = false;
-        this.checkAllDay = false;
-        this.setPaymentInput.days="";
-  
-  
-      }
-    }
-
-  onCloseCancel(){
+  onCloseCancel() {
     this.thisDialogRef.close('Cancel');
   }
 
--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -43,9 +43,10 @@ export class CustomerComponent implements OnInit {
     customerViews = true;
     pointsViews = false;
     advanceAmountViews = false;
+    paymentsDueView = false;
     salesTeamLogin = true;
     customersCount: number = 0;
-    filterInput = { "root": { "userid": this.authenticationService.loggedInUserId(), "usertype": this.authenticationService.userType(), "searchtype": "name", "searchtext": "", "lastcustomerid": "0", "pagesize": "50", "apptype": this.authenticationService.appType() } };
+    filterInput = { "root": { "userid": this.authenticationService.loggedInUserId(), "usertype": this.authenticationService.userType(), "searchtype": "name", "searchtext": "", "lastcustomerid": "0", "pagesize": "50", "apptype": this.authenticationService.appType(), userpoints: null } };
     FilterTypeDetails = [
         { value: 'alias', viewValue: 'Alias' },
         { value: 'name', viewValue: 'Name' },
@@ -55,7 +56,8 @@ export class CustomerComponent implements OnInit {
         { value: 'customertype', viewValue: 'Customer Type' },
         { value: 'followupdate', viewValue: 'Followup Date' },
         { value: 'points', viewValue: 'Points' },
-        { value: 'advamt', viewValue: 'Adv Amt < available cans' }
+        { value: 'advamt', viewValue: 'Adv Amt < available cans' },
+        { value: 'paymentsdue', viewValue: 'Payments Due' }
     ];
 
     sortCustomer = {
@@ -311,11 +313,20 @@ export class CustomerComponent implements OnInit {
             this.customerViews = false;
             this.pointsViews = false;
             this.advanceAmountViews = true;
+            this.paymentsDueView = false;
         }
         if (this.filterInput.root.searchtype == 'points') {
             this.customerViews = false;
             this.pointsViews = true;
             this.advanceAmountViews = false;
+            this.paymentsDueView = false;
+            this.filterInput.root.userpoints = null;
+        }
+        if (this.filterInput.root.searchtype == 'paymentsdue') {
+            this.customerViews = false;
+            this.pointsViews = false;
+            this.advanceAmountViews = false;
+            this.paymentsDueView = true;
         }
         let input = this.filterInput;
         // if (this.filterInput.root.searchtext && this.filterInput.root.searchtext.length > 2) {
@@ -324,12 +335,17 @@ export class CustomerComponent implements OnInit {
             let lastCustomer: any = _.last(this.customerList);
             if (lastCustomer) {
                 input.root.lastcustomerid = lastCustomer.userid;
+                if (this.filterInput.root.searchtype == 'points') {
+                    input.root.userpoints = lastCustomer.customerpoints;
+                }
+
             }
         }
         else {
             this.customerList = [];
             input.root.lastcustomerid = "0";
         }
+        console.log(input , 'sdfsdfd dfsa d')
         this.customerService.searchCustomer(input)
             .subscribe(
                 output => this.getCustomerByFilterResult(output),
@@ -400,9 +416,10 @@ export class CustomerComponent implements OnInit {
         this.followUpdate = null;
         this.customerClickMore = true;
         this.customerViews = true;
+        this.paymentsDueView = false;
         this.pointsViews = false;
         this.advanceAmountViews = false;
-        this.filterInput = { "root": { "userid": this.authenticationService.loggedInUserId(), "usertype": this.authenticationService.userType(), "searchtype": "name", "searchtext": "", "lastcustomerid": "0", "pagesize": "50", "apptype": this.authenticationService.appType() } };
+        this.filterInput = { "root": { "userid": this.authenticationService.loggedInUserId(), "usertype": this.authenticationService.userType(), "searchtype": "name", "searchtext": "", "lastcustomerid": "0", "pagesize": "50", "apptype": this.authenticationService.appType(), userpoints: null } };
         this.getCustomerList(true);
 
 
